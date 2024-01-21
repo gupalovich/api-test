48df470f05edb06e6fb2e4c9cda2bf063c4951d7
Dmitry G <gupalo1108@gmail.com>
2024-01-21 12:06:40 +0300

    cors

--- src/settings.py
+++ src/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 THIRD_PARTY_APPS = [
+    "corsheaders",
     "django_extensions",
     "graphene_django",
 ]
@@ -49,6 +50,7 @@ THIRD_PARTY_APPS = [
 INSTALLED_APPS += THIRD_PARTY_APPS
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -130,3 +132,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
